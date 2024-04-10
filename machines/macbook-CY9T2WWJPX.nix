d3999c167096f01bbdb5ed9a703fda86eb5d194c
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-10 15:45:18 +0900

    Add a new machine

---
+++ machines/macbook-CY9T2WWJPX.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }: {
+  nix = {
+    useDaemon = true;
+    gc = {
+      user = "root";
+      automatic = true;
+      interval = { Weekday = 0; Hour = 2; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
+    extraOptions = ''
+      experimental-features = nix-command flakes repl-flake
+      keep-outputs = true
+      keep-derivations = true
+      max-jobs = auto
+      extra-nix-path = nixpkgs=flake:nixpkgs
+    '';
+  };
+  programs.zsh= {
+    enable = true;
+    shellInit = ''
+      # Nix
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+      # End Nix
+      '';
+  };
+  environment.shells = with pkgs; [ bashInteractive zsh ];
+  environment.systemPackages = with pkgs; [ ];
+}
