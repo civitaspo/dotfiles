68cca60dfe52f97c20b1615d91f65b5ba06521d7
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-13 07:48:49 +0900

    Add more plugins to nvim

--- users/takahiro.nakayama/xdg/nvim/lua/config/lazy.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/config/lazy.lua
@@ -17,6 +17,16 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.ruby" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "plugins" },
   },
   defaults = {
