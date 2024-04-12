b41dcdc950952b269cf6edb5ab1f6c844c66feca
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-13 06:25:58 +0900

    Clear zellij keybind

--- users/takahiro.nakayama/programs/zellij.nix
+++ users/takahiro.nakayama/programs/zellij.nix
@@ -4,6 +4,10 @@
   zellij.enable = true;
   zellij.enableZshIntegration = true;
   zellij.settings = {
+    scroll_buffer_size = 1000000;
+    copy_command = "pbcopy";
+    copy_on_select = true;
+    "keybinds clear-defaults=true" = {};
 
   };
 }
