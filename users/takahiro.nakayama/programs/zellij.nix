dd08fe4a9213a16291cb83cd6bdb74eab431b1af
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-13 12:11:24 +0900

    Setup zellij keybind

--- users/takahiro.nakayama/programs/zellij.nix
+++ users/takahiro.nakayama/programs/zellij.nix
@@ -7,7 +7,15 @@
     scroll_buffer_size = 1000000;
     copy_command = "pbcopy";
     copy_on_select = true;
-    "keybinds clear-defaults=true" = {};
-
+    default_mode = "locked";
+    keybinds = {
+      unbind = "Ctrl g";
+      normal = {
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+      };
+      locked = {
+        "bind \"Ctrl z\"" = { SwitchToMode = "normal"; };
+      };
+    };
   };
 }
