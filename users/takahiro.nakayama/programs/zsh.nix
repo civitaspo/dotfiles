d2e6bd098a214e2cb35222a7090a67c2c9443943
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-13 12:09:17 +0900

    Update zsh settings

--- users/takahiro.nakayama/programs/zsh.nix
+++ users/takahiro.nakayama/programs/zsh.nix
@@ -17,21 +17,85 @@
     {
       name = "zsh-autosuggestions";
       src = pkgs.zsh-autosuggestions;
+      file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
     }
     {
       name = "zsh-syntax-highlighting";
       src = pkgs.zsh-fast-syntax-highlighting;
-    }
-    {
-      name = "zsh-completions";
-      src = pkgs.zsh-completions;
+      file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
     }
   ];
+  zsh.sessionVariables = {
+    WORDCHARS = "*?_-.[]~!#$%^(){}<>";
+  };
   zsh.shellAliases = {
-    gst = "${pkgs.git}/bin/git status -s";
+    gst = "${pkgs.git}/bin/git status";
     ga = "${pkgs.git}/bin/git add";
     gc = "${pkgs.git}/bin/git commit";
+    gb = "${pkgs.git}/bin/git branch";
     grs = "${pkgs.git}/bin/git restore";
+    gsw = "${pkgs.git}/bin/git switch";
     vim = "${pkgs.neovim}/bin/nvim";
+    ls = "${pkgs.eza}/bin/eza";
+    diff = "${pkgs.delta}/bin/delta";
   };
+  zsh.initExtra = ''
+  setopt correct
+  setopt correct_all
+  setopt share_history
+  setopt sh_word_split
+  setopt rc_quotes
+  setopt auto_remove_slash
+  setopt no_beep
+  setopt no_list_beep
+  setopt no_hist_beep
+  setopt path_dirs
+  setopt print_exit_value
+  setopt notify
+  setopt no_case_glob
+  setopt mark_dirs
+  setopt auto_cd
+  setopt auto_menu
+  setopt auto_param_keys
+  setopt auto_param_slash
+  setopt auto_pushd
+  setopt complete_in_word
+  setopt globdots
+  setopt interactive_comments
+  setopt list_types
+  setopt magic_equal_subst
+  setopt prompt_subst
+
+  umask 022
+
+  autoload -Uz url-quote-magic
+  zle -N self-insert url-quote-magic
+
+
+  typeset -gx -U path
+  path=( \
+      ${config.home.homeDirectory}/bin(N-/) \
+      ${config.home.homeDirectory}/sbin(N-/) \
+      ${config.home.homeDirectory}/scripts(N-/) \
+      ${config.home.homeDirectory}/.local/share/aquaproj-aqua/bin(N-/) \
+      ${config.home.homeDirectory}/.rd/bin(N-/) \
+      "$path[@]" \
+    )
+
+  frepo() {
+    local dir
+    dir=$(${pkgs.ghq}/bin/ghq list > /dev/null | ${pkgs.fzf}/bin/fzf +m) &&
+      \cd $(${pkgs.ghq}/bin/ghq root)/$dir
+    zle accept-line
+  }
+  zle -N frepo
+  bindkey '^g' frepo
+  '';
+  zsh.initExtraBeforeCompInit = ''
+  typeset -gx -U fpath
+  fpath=( \
+      ${pkgs.zsh-completions}/share/zsh/site-functions(N-/) \
+      $fpath \
+      )
+  '';
 }
