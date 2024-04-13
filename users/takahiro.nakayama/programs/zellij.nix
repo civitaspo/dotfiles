0d4ed73c9f19c25301705ae43af06c28592b6c47
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-14 08:08:09 +0900

    Modify zellij keybinding
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/programs/zellij.nix
+++ users/takahiro.nakayama/programs/zellij.nix
@@ -9,9 +9,41 @@
     copy_on_select = true;
     default_mode = "locked";
     keybinds = {
-      unbind = "Ctrl g";
+      # nix `toKDL` is so buggy behavior, so this settings may be broken in the future.
+      "unbind \"Ctrl g\"" = [];
       normal = {
+        "unbind \"Ctrl n\"" = [];
+        "unbind \"Ctrl h\"" = [];
         "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Ctrl r\"" = { SwitchToMode = "resize"; };
+        "bind \"Ctrl m\"" = { SwitchToMode = "move"; };
+        "bind \"p\"" = { NewPane = "Right"; };
+        "bind \"w\"" = { FocusNextPane = []; };
+        "bind \"t\"" = { NewTab = []; };
+        "bind \"n\"" = { GoToNextTab = []; };
+      };
+      tab = {
+        "unbind \"Ctrl t\"" = [];
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Esc\" \"Esc\"" = { SwitchToMode = "locked"; };
+      };
+      pane = {
+        "unbind \"Ctrl p\"" = [];
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Esc\" \"Esc\"" = { SwitchToMode = "locked"; };
+      };
+      move = {
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Esc\" \"Esc\"" = { SwitchToMode = "locked"; };
+      };
+      search = {
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Esc\" \"Esc\"" = { SwitchToMode = "locked"; };
+      };
+      session = {
+        "bind \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Ctrl z\" \"Ctrl z\"" = { SwitchToMode = "locked"; };
+        "bind \"Esc\" \"Esc\"" = { SwitchToMode = "locked"; };
       };
       locked = {
         "bind \"Ctrl z\"" = { SwitchToMode = "normal"; };
