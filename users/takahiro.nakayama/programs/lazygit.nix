7e11ab6487b888cff24b9837ceb5d65f66438c4c
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-13 20:09:11 +0900

    Setup lazygit keybind

--- users/takahiro.nakayama/programs/lazygit.nix
+++ users/takahiro.nakayama/programs/lazygit.nix
@@ -2,5 +2,34 @@
 
 {
   lazygit.enable = true;
-  lazygit.settings = {};
+  lazygit.settings = {
+    # https://github.com/jesseduffield/lazygit/blob/145fb6191c108f7e7af8180106b82f0503a7994c/docs/Config.md
+    git = {
+      paging = {
+        colorArg = "always";
+        pager = "${pkgs.delta}/bin/delta --paging=never";
+      };
+      commit = {
+        signOff = true;
+      };
+    };
+    keybinding = {
+      universal = {
+        prevItem = "<c-p>";
+        nextItem = "<c-n>";
+        prevBlock = "<c-b>";
+        nextBlock = "<c-f>";
+        gotoTop = "<c-t>";
+        gotoBottom = "G";
+        pushFiles = "P";
+        pullFiles = "p";
+        createPatchOptionsMenu = "<c-x>";
+        diffingMenu-alt = "";
+      };
+      files = {
+        findBaseCommitForFixup = "";
+        openStatusFilter = "";
+      };
+    };
+  };
 }
