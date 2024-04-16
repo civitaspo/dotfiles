20b57973a4c3e362c6913fc4207ae57172c42a7b
Takahiro Nakayama <civitaspo@gmail.com>
2024-04-16 11:14:14 +0900

    Set overrideGpg = true to lazygit
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/programs/lazygit.nix
+++ users/takahiro.nakayama/programs/lazygit.nix
@@ -5,6 +5,7 @@
   lazygit.settings = {
     # https://github.com/jesseduffield/lazygit/blob/145fb6191c108f7e7af8180106b82f0503a7994c/docs/Config.md
     git = {
+      overrideGpg = true;
       paging = {
         colorArg = "always";
         pager = "${pkgs.delta}/bin/delta --paging=never";
