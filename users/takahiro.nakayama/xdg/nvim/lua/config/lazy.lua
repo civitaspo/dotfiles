f5c32675b8b5a3d8df6936d6a244eb2244b1289c
Takahiro Nakayama <civitaspo@gmail.com>
2024-05-07 07:33:00 +0900

    Remove un-used extras
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/config/lazy.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/config/lazy.lua
@@ -19,10 +19,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.editor.aerial" },
-    { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.editor.navic" },
-    { import = "lazyvim.plugins.extras.editor.outline" },
     { import = "lazyvim.plugins.extras.editor.trouble-v3" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.lang.docker" },
