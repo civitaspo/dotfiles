5b3cbfd2bc671c67383c287e29b9421784434570
Takahiro Nakayama <civitaspo@gmail.com>
2024-05-07 16:18:20 +0900

    noremap
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/config/keymaps.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/config/keymaps.lua
@@ -2,16 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set({'i', 'v', 'n'}, '<C-f>', '<Right>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-b>', '<Left>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-n>', '<Down>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-p>', '<Up>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-e>', '<End>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-a>', '<Home>')
-vim.keymap.set({'i', 'v', 'n'}, '<C-h>', '<BS>')
-vim.keymap.set({'i', 'v'}, '<C-d>', '<Del>') -- <C-d> in normal mode is for scrolling down
-vim.keymap.set('i', '<C-k>', '<C-o>D')
-vim.keymap.set('i', '<C-u>', '<C-o>d^')
-vim.keymap.set('i', '<C-w>', '<C-o>db')
-vim.keymap.set('n', '<C-c>', ':q<CR>')
-vim.keymap.set('n', '<C-x>', ':wq<CR>')
+vim.keymap.set({ "i", "v", "n" }, "<C-f>", "<Right>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-b>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-n>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-p>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-e>", "<End>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-a>", "<Home>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v", "n" }, "<C-h>", "<BS>", { noremap = true, silent = true })
+vim.keymap.set({ "i", "v" }, "<C-d>", "<Del>", { noremap = true, silent = true }) -- <C-d> in normal mode is for scrolling down
+vim.keymap.set("i", "<C-k>", "<C-o>D", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-u>", "<C-o>d^", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-w>", "<C-o>db", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-c>", ":q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-x>", ":wq<CR>", { noremap = true, silent = true })
