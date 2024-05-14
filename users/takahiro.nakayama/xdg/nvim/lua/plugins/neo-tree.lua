e3228ab5dc4f2f9ce3b1e456c0ed1acdd0e85411
Takahiro Nakayama <civitaspo@gmail.com>
2024-05-15 06:09:04 +0900

    Add never shown items & grouped_items
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/neo-tree.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/neo-tree.lua
@@ -4,17 +4,26 @@ return {
     opts = {
       filesystem = {
         filtered_items = {
-	        visible = true,
-	        show_hidden_count = true,
-	        hide_dotfiles = false,
-	        hide_gitignored = true,
-	        hide_by_name = {
-	          '.git',
-	          '.DS_Store',
-	        },
-	        never_show = {},
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            '.git',
+          },
+          hide_by_pattern = {},
+          never_show = {
+            '.DS_Store',
+            '.terraform',
+          },
+          never_show_by_pattern = {
+            '.git/logs/**',
+            '.git/objects/**',
+            '.git/refs/**',
+          },
         },
       },
+      group_empty_dirs = true,
     },
   },
 }
