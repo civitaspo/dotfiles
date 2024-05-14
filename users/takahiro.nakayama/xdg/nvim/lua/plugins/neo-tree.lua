e914002f9cebe227ce230df3589c6a226ed13d48
Takahiro Nakayama <civitaspo@gmail.com>
2024-05-15 06:51:14 +0900

    Hmm...
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/neo-tree.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/neo-tree.lua
@@ -15,8 +15,13 @@ return {
           never_show = {
             '.DS_Store',
             '.terraform',
+            -- TODO: The below works, but 'never_show_by_pattern' does not. Why?
+            'logs',
+            'objects',
+            'refs'
           },
           never_show_by_pattern = {
+            -- TODO: Why does this not work?
             '.git/logs/**',
             '.git/objects/**',
             '.git/refs/**',
