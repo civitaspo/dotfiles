121772595c17b817179cc29d585dbadb6946e6c9
Takahiro Nakayama <civitaspo@gmail.com>
2024-05-19 20:55:33 +0900

    "echasnovski/mini.indentscope" is disabled by default
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
@@ -1,7 +1,6 @@
 return {
   -- NOTE: Disable UI plugins to avoid conflicts with hlchunk.nvim.
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
   {
     "shellRaining/hlchunk.nvim",
     event = { "UIEnter" },
