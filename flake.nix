47523879bcb361c4687c2ff85b2e39a191f2163e
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-09 08:40:09 +0900

    Use nixpkgs-24.05 instead of 23.11
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "NixOS systems and tools by civitaspo";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
