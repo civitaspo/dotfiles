a2ed2478a9669a8b7dfe938c3138744d1c3880ea
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-09 15:17:58 +0900

    Enable HLchunk
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
@@ -3,12 +3,24 @@ return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   {
     "shellRaining/hlchunk.nvim",
-    event = { "UIEnter" },
+    event = { "BufReadPre", "BufNewFile" },
     opts = {
       chunk = {
+        enable = true,
         notify = false,
       },
+      indent = {
+        enable = true,
+        notify = false,
+      },
+      line_num = {
+        enable = true,
+        notify = false,
+        style = "#edde7b",
+      },
       blank = {
+        enable = true,
+        notify = false,
         chars = {
           "⇢",
         },
