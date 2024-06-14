428c28e2eb422e80f1e4ec8561b8dda448d3935c
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-14 15:44:02 +0900

    Revert "cleanup in 7days"
    This reverts commit adc748b839a7c506032c849238adbbc7df102c00.

--- machines/macbook-CY9T2WWJPX.nix
+++ machines/macbook-CY9T2WWJPX.nix
@@ -5,7 +5,7 @@
       user = "root";
       automatic = true;
       interval = { Weekday = 0; Hour = 2; Minute = 0; };
-      options = "--delete-older-than 7d";
+      options = "--delete-older-than 30d";
     };
     extraOptions = ''
       experimental-features = nix-command flakes repl-flake
