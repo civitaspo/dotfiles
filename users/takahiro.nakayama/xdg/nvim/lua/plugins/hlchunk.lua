095a33648f7ee6ea5b387a98fb1d4e18f63c276f
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-19 15:49:54 +0900

    Fix hlchunk settings & enable indent-blankline
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/hlchunk.lua
@@ -1,6 +1,4 @@
 return {
-  -- NOTE: Disable UI plugins to avoid conflicts with hlchunk.nvim.
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   {
     "shellRaining/hlchunk.nvim",
     event = { "BufReadPre", "BufNewFile" },
@@ -9,22 +7,24 @@ return {
         enable = true,
         notify = false,
       },
-      indent = {
-        enable = false,
-        notify = false,
-      },
       line_num = {
         enable = true,
         notify = false,
         style = "#edde7b",
       },
-      blank = {
-        enable = true,
-        notify = false,
-        chars = {
-          "⇢",
-        },
-      },
+      -- NOTE: hlchunk.nvim indent & blank features are unstable. So, disable them.
+      -- NOTE: I would like to use indent-blankline.nvim instead of the hlchunk.nvim indent & blank features.
+      -- indent = {
+      --   enable = false,
+      --   notify = false,
+      -- },
+      -- blank = {
+      --   enable = true,
+      --   notify = false,
+      --   chars = {
+      --     "⇢",
+      --   },
+      -- },
     },
   },
 }
