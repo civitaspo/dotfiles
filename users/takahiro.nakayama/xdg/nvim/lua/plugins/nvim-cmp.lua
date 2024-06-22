82ccb12cc4b02d2fd1abc8078a3631fd02e7349c
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-22 17:44:58 +0900

    Goodbye S-CR
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
@@ -11,12 +11,6 @@ return {
         -- https://github.com/hrsh7th/nvim-cmp/blob/a110e12d0b58eefcf5b771f533fc2cf3050680ac/lua/cmp/config/mapping.lua#L44
         ['<C-P>'] = cmp.config.disable,
         ['<C-N>'] = cmp.config.disable,
-
-        -- Use <S-CR> instead of <C-N>
-        -- https://github.com/LazyVim/LazyVim/blob/b47c65f4087c4d82720ab7439f395aba5d6b5f40/lua/lazyvim/plugins/coding.lua#L39
-        ["<S-CR>"] = cmp.mapping(function()
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        end, { "i", "s" }),
       })
     end,
   },
