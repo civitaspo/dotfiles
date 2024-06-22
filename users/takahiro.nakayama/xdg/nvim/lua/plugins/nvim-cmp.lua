ec4309674e11c41f008f23d982da8558cf8370a0
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-22 16:53:57 +0900

    Uppercase is used in nvim-cmp
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
@@ -6,13 +6,11 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         -- I do not want to use Emacs-like keybindings for completion.
         -- Use supertab instead.
-        ['<C-f>'] = cmp.config.disable,
-        ['<C-b>'] = cmp.config.disable,
+        ['<C-F>'] = cmp.config.disable,
+        ['<C-B>'] = cmp.config.disable,
         -- https://github.com/hrsh7th/nvim-cmp/blob/a110e12d0b58eefcf5b771f533fc2cf3050680ac/lua/cmp/config/mapping.lua#L44
-        ['<C-p>'] = cmp.config.disable,
-        ['<C-n>'] = cmp.config.disable,
-        ['<Up>'] = cmp.config.disable,
-        ['<Down>'] = cmp.config.disable,
+        ['<C-P>'] = cmp.config.disable,
+        ['<C-N>'] = cmp.config.disable,
       })
     end,
   },
