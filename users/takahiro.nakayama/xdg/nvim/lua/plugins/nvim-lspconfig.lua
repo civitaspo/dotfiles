4cbad7064b486dcf2f3655bcefc28857ed7e235d
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-22 20:41:09 +0900

    Disable C-k for lspconfig
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-lspconfig.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,13 +3,7 @@ return {
     "neovim/nvim-lspconfig",
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- -- change a keymap
-      -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
-      -- -- disable a keymap
-      -- keys[#keys + 1] = { "K", false }
-      -- -- add a keymap
-      -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
-      keys[#keys + 1] = { "<C-k>", false }
+      keys[#keys + 1] = { "<C-k>", mode = "i", false }
     end,
   }
 };
