54ba216d3aaa8033a13dd7e3d6ee8e9a513e2b6a
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-22 15:35:33 +0900

    I do not want to use Emacs-like keybindings for completion.
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
@@ -2,11 +2,15 @@ return {
   {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
-      -- Disable default <C-f> and <C-b> mappings
-      -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      opts.mapping['<C-f>'] = nil
-      opts.mapping['<C-b>'] = nil
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        -- I do not want to use Emacs-like keybindings for completion.
+        -- Use supertab instead.
+        ['<C-f>'] = cmp.config.disable,
+        ['<C-b>'] = cmp.config.disable,
+        ['<C-p>'] = cmp.config.disable,
+        ['<C-n>'] = cmp.config.disable,
+      })
     end,
   },
 };
