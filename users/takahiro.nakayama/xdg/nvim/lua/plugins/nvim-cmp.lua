5864df04dceb10a22f8f330894f153785e3af91b
Takahiro Nakayama <civitaspo@gmail.com>
2024-06-22 16:27:15 +0900

    Disable Up/Down for cmp
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
+++ users/takahiro.nakayama/xdg/nvim/lua/plugins/nvim-cmp.lua
@@ -8,8 +8,11 @@ return {
         -- Use supertab instead.
         ['<C-f>'] = cmp.config.disable,
         ['<C-b>'] = cmp.config.disable,
+        -- https://github.com/hrsh7th/nvim-cmp/blob/a110e12d0b58eefcf5b771f533fc2cf3050680ac/lua/cmp/config/mapping.lua#L44
         ['<C-p>'] = cmp.config.disable,
         ['<C-n>'] = cmp.config.disable,
+        ['<Up>'] = cmp.config.disable,
+        ['<Down>'] = cmp.config.disable,
       })
     end,
   },
