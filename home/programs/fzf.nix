8708b045c151e9a16ea1ce988996b49c959f079d
Takahiro Nakayama <civitaspo@gmail.com>
2025-01-25 12:02:29 +0900

    Setup fzf defaultOptions
    Signed-off-by: Takahiro Nakayama <civitaspo@gmail.com>

--- home/programs/fzf.nix
+++ home/programs/fzf.nix
@@ -3,6 +3,9 @@
 {
   fzf.enable = true;
   fzf.enableZshIntegration = true;
+  fzf.defaultOptions = [
+    "--bind ctrl-n:down,ctrl-p:up"
+  ];
   # TODO: fd
   # fzf.changeDirWidgetCommand = "fd --type d";
   # fzf.fileWidgetCommand = "fd --type f";
