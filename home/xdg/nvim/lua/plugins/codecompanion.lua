c234d1ef62403a58fdd027440ebe31add3f8fcc1
civitaspo <civitaspo@gmail.com>
2025-09-07 12:57:47 +0900

    Introduce codecompanion

---
+++ home/xdg/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    opts = {
+      --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+      strategies = {
+        chat = { adapter = "claude_code" },
+        inline = { adapter = "copilot" },
+      },
+    },
+  },
+};
