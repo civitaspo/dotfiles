e6104b4fc8368fd1d23768661dfd41cd3ee63859
civitaspo <civitaspo@gmail.com>
2025-09-07 13:52:15 +0900

    workaround: https://github.com/LazyVim/LazyVim/pull/6354

--- home/xdg/nvim/lua/plugins/colorscheme.lua
+++ home/xdg/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
-  { "catppuccin/nvim" },
+  {
+    "catppuccin/nvim",
+    -- https://github.com/LazyVim/LazyVim/pull/6354
+    optional = true,
+    opts = function()
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
