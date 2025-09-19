41441b90a855ea8088f4a6c376fc61b9238d0b63
civitaspo <civitaspo@gmail.com>
2025-09-19 11:48:50 +0900

    Remove catppuccin workaround because the issue is resolved
    Signed-off-by: civitaspo <civitaspo@gmail.com>

--- home/xdg/nvim/lua/plugins/colorscheme.lua
+++ home/xdg/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,5 @@
 return {
-  {
-    "catppuccin/nvim",
-    -- https://github.com/LazyVim/LazyVim/pull/6354
-    optional = true,
-    opts = function()
-      local bufferline = require("catppuccin.groups.integrations.bufferline")
-      bufferline.get = bufferline.get or bufferline.get_theme
-    end,
-  },
+  { "catppuccin/nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
