67644956bbf838e3b50f459bf66413f1d37554da
civitaspo <civitaspo@gmail.com>
2025-10-01 14:20:04 +0900

    Use lazyvim default settings as possible
    Signed-off-by: civitaspo <civitaspo@gmail.com>

--- home/xdg/nvim/lua/plugins/sidekick.lua
+++ home/xdg/nvim/lua/plugins/sidekick.lua
@@ -1,34 +1,7 @@
 return {
   {
     "folke/sidekick.nvim",
-    opts = {
-      -- add any options here
-      cli = {
-        mux = {
-          enabled = false,
-        },
-      },
-    },
     keys = {
-      {
-        "<tab>",
-        function()
-          -- if there is a next edit, jump to it, otherwise apply it if any
-          if not require("sidekick").nes_jump_or_apply() then
-            return "<Tab>" -- fallback to normal tab
-          end
-        end,
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
-      },
-      {
-        "<c-.>",
-        function()
-          require("sidekick.cli").focus()
-        end,
-        desc = "Sidekick Switch Focus",
-        mode = { "n", "v" },
-      },
       {
         "<c-,>",
         function()
@@ -36,38 +9,6 @@ return {
         end,
         desc = "Sidekick Toggle CLI",
         mode = { "n", "v" },
-      },
-      {
-        "<leader>ac",
-        function()
-          require("sidekick.cli").toggle({ name = "claude", focus = true })
-        end,
-        desc = "Sidekick Claude Toggle",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ag",
-        function()
-          require("sidekick.cli").toggle({ name = "grok", focus = true })
-        end,
-        desc = "Sidekick Grok Toggle",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>ap",
-        function()
-          require("sidekick.cli").select_prompt()
-        end,
-        desc = "Sidekick Ask Prompt",
-        mode = { "n", "v" },
-      },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { "<leader>a", group = "ai" },
       },
     },
   },
