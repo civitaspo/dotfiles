60ed196ec3c92045053a9b5574870fa8d506cd66
civitaspo <civitaspo@gmail.com>
2025-10-28 17:40:57 +0900

    do not intercept c-f / c-b in mode t
    Signed-off-by: civitaspo <civitaspo@gmail.com>

--- home/xdg/nvim/lua/plugins/sidekick.lua
+++ home/xdg/nvim/lua/plugins/sidekick.lua
@@ -9,11 +9,13 @@ return {
             width = 105, -- 90~110 is good for me.
           },
           keys = {
+            buffers = { "<c-b>", "buffers", mode = "n", desc = "open buffer picker" },
+            files = { "<c-f>", "files", mode = "n", desc = "open file picker" },
             prompt = { "<c-.>", "prompt" },
-            nav_left      = { "<c-,>", "nav_left"  , expr = true, desc = "navigate to the left window" },
-            nav_down      = nil,
-            nav_up        = { "<c-,>", "nav_up"    , expr = true, desc = "navigate to the above window" },
-            nav_right     = nil,
+            nav_left = { "<c-,>", "nav_left", expr = true, desc = "navigate to the left window" },
+            nav_down = nil,
+            nav_up = { "<c-,>", "nav_up", expr = true, desc = "navigate to the above window" },
+            nav_right = nil,
           },
         },
       },
